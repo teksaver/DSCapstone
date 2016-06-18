0a1351832a261cd4f37dbc02147949dca9bc89ae
Sylvain Tenier <sylvain@iMac-de-Sylvain-Tenier.local>
2016-06-18 17:32:54 +0200

    test with decision trees

--- predictionQuizz1.R
+++ predictionQuizz1.R
@@ -9,20 +9,20 @@ set.seed(1979)
 # File loading with sampling
 
 blogsConn <- file("./final/en_US/en_US.blogs.txt","r")
-#newsConn <- file("./final/en_US/en_US.news.txt","r")
+newsConn <- file("./final/en_US/en_US.news.txt","r")
 #twittConn <- file("./final/en_US/en_US.twitter.txt","r")
 blogsLines <- readLines(blogsConn,n=-1,skipNul = TRUE)
-#newsLines <- readLines(newsConn,n=-1,skipNul = TRUE)
+newsLines <- readLines(newsConn,n=-1,skipNul = TRUE)
 #twittLines <- readLines(twittConn,n=-1,skipNul = TRUE)
 close(blogsConn)
-#close(newsConn)
+close(newsConn)
 #close(twittConn)
 blogsSubset <- blogsLines[sample(1:length(blogsLines), length(blogsLines)/500, replace=FALSE)]
-#newsSubset <- blogsLines[sample(1:length(newsLines), length(newsLines)/500, replace=FALSE)]
+newsSubset <- blogsLines[sample(1:length(newsLines), length(newsLines)/500, replace=FALSE)]
 #twittSubset <- blogsLines[sample(1:length(twittLines), length(twittLines)/500, replace=FALSE)]
 
 #sample corpus loading
-myCorpus <- corpus(blogsSubset)
+myCorpus <- corpus(newsSubset) #+ corpus(newsSubset)
 metadoc(myCorpus, "language") <- "english"
 summary(myCorpus, n = 5)
 
@@ -30,37 +30,46 @@ summary(myCorpus, n = 5)
 profanity <- scan("naughty.txt", what="", sep="\n")
 
 #create unigrams
-unigrams.dfm <- dfm(myCorpus, ngrams = 1, ignoredFeatures = c(profanity, stopwords("english")),
+unigrams.dfm <- dfm(myCorpus, ngrams = 1, ignoredFeatures = c(profanity),
                     removePunct = TRUE, removeNumbers = TRUE,
                     removeTwitter = TRUE, removeSeparators = TRUE, removeHyphens = TRUE, stem = TRUE)
 topfeatures(unigrams.dfm)
 
 #create bigrams
-bigrams.dfm <- dfm(myCorpus, ngrams = 2, ignoredFeatures = c(profanity, stopwords("english")),
+bigrams.dfm <- dfm(myCorpus, ngrams = 2, ignoredFeatures = c(profanity),
                     removePunct = TRUE, removeNumbers = TRUE,
                     removeTwitter = TRUE, removeSeparators = TRUE, removeHyphens = TRUE, stem = TRUE)
 topfeatures(bigrams.dfm)
 
 #create trigrams
-trigrams.dfm <- dfm(myCorpus, ngrams = 3, ignoredFeatures = c(profanity, stopwords("english")),
+trigrams.dfm <- dfm(myCorpus, ngrams = 3, ignoredFeatures = c(profanity),
                    removePunct = TRUE, removeNumbers = TRUE,
                    removeTwitter = TRUE, removeSeparators = TRUE, removeHyphens = TRUE, stem = TRUE)
 topfeatures(trigrams.dfm)
-
-trigrams.df <- as.data.frame(trigrams.dfm)
 
 #tests tidyr
 
 library(dplyr)
 library(tidyr)
-long.df <- trigrams.df %>% gather("trigram","occurences") %>%
+long.df <- as.data.frame(trigrams.dfm) %>%
+    gather("trigram","occurences") %>%
     filter(occurences!=0)
-long.df <- data.frame(trigram = rep(long.df$trigram, long.df$occurences))
-long.df$x <- gsub("\\_[^_]*$","",long.df$trigram)
-long.df$y <- gsub("^.*\\_","", long.df$trigram )
+long.df <- data.frame(known = rep(long.df$trigram, long.df$occurences))
+long.df$y <- gsub("^.*\\_","", long.df$known )
+long.df$known <- gsub("\\_[^_]*$","",long.df$known)
 
 #training
-library(caret)
-model <- train(y ~x, data=long.df,
-                    method='rpart')
+library(tree)
+tree.training <- tree(y~., long.df)
 
+testQuizz1 <- "The guy in front of me just bought a pound of bacon, a bouquet, and a case of"
+
+testQuizz2 <- "You're the reason why I smile everyday. Can you follow me please? It would mean the"
+
+testCorpus <- corpus(testQuizz1)
+testCorpusBigrams.dfm <- dfm(testCorpus, ngrams = 2, ignoredFeatures = c(profanity),
+                   removePunct = TRUE, removeNumbers = TRUE,
+                   removeTwitter = TRUE, removeSeparators = TRUE, removeHyphens = TRUE, stem = TRUE)
+
+topfeatures(testCorpusBigrams.dfm)
+as.data.frame(testCorpusBigrams.dfm)
